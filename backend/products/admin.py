a7227623679b4066c90c938aa565cc8681ea16a4
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-07-24 19:38:33 +0500

    Added pagination on backend for list apis

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -6,6 +6,6 @@ from products.models import Product
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "price", "created_at")
+    list_display = ("name", "price", "created_at", "product_type")
     list_filter = ("created_at",)
     search_fields = ("name", "description")
